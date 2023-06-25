c9667af139a9f785e81c64c38bde32b46ee1b418
Nilon123456789 <dowers.purer0q@icloud.com>
2023-06-25 11:51:16 -0400

    ADD - Queue to parse input data

--- BLE/central/serreiot/reader.py
+++ BLE/central/serreiot/reader.py
@@ -1,7 +1,7 @@
 from threading import Thread
-import serial
-import re
-import codecs
+from queue import Queue
+from time import sleep
+import serial, re
 
 from device import Device
 
@@ -11,10 +11,15 @@ class Reader():
         self.__ser = serial.Serial(port, baudrate)
         self.__send_data_cb = send_data_cb
         self.__send_logs_cb = send_logs_cb
+        self.__input_buffer = Queue() 
         self.__devices = {}
+        self.__sleep_time = 0.01
 
-        self.__thread = Thread(target=self.__read)
-        self.__thread.start()
+        self.__read_thread = Thread(target=self.__read, daemon=True)
+        self.__read_thread.start()
+
+        self.__input_buffer_parser_thread = Thread(target=self.__input_buffer_parser)
+        self.__input_buffer_parser_thread.start()
 
     def __read(self):
         while(True):
@@ -29,6 +34,7 @@ class Reader():
                     err = f"[Error] Decoding error occurred at {error_position} for line: {line}"
                     
             if line == "": # Check if the line is empty
+                sleep(self.__sleep_time)
                 continue
 
             if err != "": # Check if there is an error
@@ -36,29 +42,49 @@ class Reader():
             
             if line[0] == "[" : # Check if the line is a log
                 self.__send_logs_cb(line)
+                sleep(self.__sleep_time)
                 continue
             
             if not self.__is_valid(line): # Check if the data is valid
+                sleep(self.__sleep_time)
                 continue
-            
+
+            # Add the data to the input buffer so it's treated in order
+            self.__input_buffer.put(line)
+
+    
+    def __input_buffer_parser(self) -> None:
+        '''Parse the input buffer'''
+        while True:
+            if  self.__input_buffer.empty(): # Check if the input buffer is empty
+                sleep(self.__sleep_time)
+                continue
+ 
+            line = self.__input_buffer.get(0) # Get the data from the input buffer
+
             print("\033[32mDATA: {}\033[0m".format(line))
 
             device = Device(line) # Create a new device from the data
 
             if device.id == -1: # Check if the device is valid
+                self.__send_logs_cb(f"[Error] The device is not valid for line: {line}")
+                sleep(self.__sleep_time)
                 continue
             
             if device.addr not in self.__devices: # Check if the device is already in the list
                 self.__devices[device.addr] = device # Add the device to the list
                 self.__send_data_cb(device) # Send the data
+                sleep(self.__sleep_time)
                 continue
             
             # Check if the device has not the same id as last time
             if device.id > self.__devices[device.addr].id or abs(device.id - self.__devices[device.addr].id) > 5:
                 self.__send_data_cb(device) # Send the data
                 self.__devices[device.addr] = device # Update the device
+                sleep(self.__sleep_time)
                 continue
-
+            
+            sleep(self.__sleep_time)
             continue
 
     def __is_valid(self, data) -> bool:
